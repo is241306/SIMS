1253a7725da6df9cc655d33143551133daaaaa5e
Filip Mielnikowicz <filipmiiele@gmail.com>
2025-11-15 18:15:23 +0100

    Frontend Startover since MudBlazor is trash

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,13 +1,13 @@
 using frontend.Components;
-using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddMudServices();
+
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()){
     app.UseExceptionHandler("/Error", createScopeForErrors: true);

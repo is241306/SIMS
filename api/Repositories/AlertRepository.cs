463b341690517fd28d413d940eefde76cfff718c
Roman Warter <is241315@fhstp.ac.at>
2025-11-16 17:38:49 +0100

    Models adjusted and Repisotories created and SimsContext adjusted

---
+++ api/Repositories/AlertRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using api.Data;
+using api.Models;
+
+namespace api.Repositories;
+
+public class AlertRepository : IAlertRepository
+{
+    private readonly SimsContext _context;
+
+    public AlertRepository(SimsContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Alert?> GetByIdAsync(int id)
+    {
+        return await _context.Alerts
+            .Include(a => a.Incidents)   // if Alert has navigation property Incidents
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task<IReadOnlyList<Alert>> GetAllAsync()
+    {
+        return await _context.Alerts
+            .Include(a => a.Incidents)   // remove this Include if you do not want incidents every time
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(Alert alert)
+    {
+        await _context.Alerts.AddAsync(alert);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Alert alert)
+    {
+        _context.Alerts.Remove(alert);
+        await _context.SaveChangesAsync();
+    }
+}

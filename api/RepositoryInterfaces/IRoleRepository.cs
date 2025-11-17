db7dfeed866bd496f271cde9cea2ad0e604979e9
KonstantinVlaykovski <56672782+KonstantinVlaykovski@users.noreply.github.com>
2025-11-17 22:35:49 +0100

    Created RepositoryInterfaces for the Repositories

---
+++ api/RepositoryInterfaces/IRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using api.Models;
+
+namespace api.Repositories
+
+{
+    public interface IRoleRepository
+    {
+        Task<Role?> GetByIdAsync(int id);
+        Task<Role?> GetByNameAsync(string name);
+        Task<IReadOnlyList<Role>> GetAllAsync();
+    }
+}
